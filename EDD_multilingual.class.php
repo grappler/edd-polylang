6cd5b4b0d20429cd54b0b53673aada0322cfbc25
dgwatkins <dgwatkins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 15:39:32 +0000

    Save langauge of payment and user it later to send email receipts
    git-svn-id: https://plugins.svn.wordpress.org/edd-multilingual/trunk@634059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- EDD_multilingual.class.php
+++ EDD_multilingual.class.php
@@ -8,9 +8,16 @@ class EDD_multilingual{
     function init() {
         if (!defined('ICL_SITEPRESS_VERSION') || !defined('EDD_PLUGIN_DIR')) {
             add_action('admin_notices', array(&$this, 'error_no_plugins'));
-        } elseif (!is_admin()) {
-            $this->translate_pages();
+            return;
         }
+
+        if (!is_admin()) {
+            $this->translate_pages();
+        } else {
+            add_filter('edd_get_payment_meta', array(&$this, 'email_receipt_language'));
+        }
+        add_action('edd_insert_payment', array(&$this, 'save_payment_language'), 10, 2);
+
     }
 
     function error_no_plugins() {
@@ -19,6 +26,22 @@ class EDD_multilingual{
             <p><strong><?php printf(__('EDD multilingual only works when %s and %s are installed and active.', 'edd_multilingual'), 'WPML', 'Easy Digital Downloads'); ?></strong></p>
         </div>
         <?php
+    }
+
+    function save_payment_language($payment, $payment_data) {
+        global $sitepress;
+        update_post_meta($payment, 'wpml_language', $sitepress->get_current_language());
+    }
+
+    function email_receipt_language($meta) {
+        if (isset($_GET['edd-action']) && $_GET['edd-action'] == 'email_links') {
+            $lang = get_post_meta(intval($_GET['purchase_id']), 'wpml_language', true);
+            if (!empty($lang)) {
+                global $sitepress;
+                $sitepress->switch_lang($lang);
+            }
+        }
+        return $meta;
     }
 
     function translate_pages() {
